6d5b30ac0241463b093cddecdad5b842f0618b6e
balamuralicl <balamurali999cl@gmail.com>
2025-06-17 12:15:07 -0700

    intial

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Query
+import requests
+import os
+
+# Load .env locally, skip if not found (e.g., in EC2)
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
+app = FastAPI()
+
+API_KEY = os.getenv("API_KEY")  # Will work from .env locally or from GitHub Actions on EC2
+
+print(API_KEY)
+
+@app.get("/weather")
+def get_weather(lat: float = Query(...), lon: float = Query(...)):
+    if not API_KEY:
+        return {"error": "API key missing"}
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&units=metric"
+    print(f"Calling: {url}")
+    response = requests.get(url)
+    return response.json()
